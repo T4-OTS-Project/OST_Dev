a6ce3f593bcab9288560838116cde40941f36eb4
Viktoriia Minosian <116222974+Vikmin2022@users.noreply.github.com>
2023-04-15 13:31:41 -0500

    Update main.tf

--- modules/Namespace/main.tf
+++ modules/Namespace/main.tf
@@ -3,3 +3,11 @@ module ots-ns {
     name = "ots-ns"
 }
     
+terraform {
+  backend "gcs" {
+    bucket  = "terraform-project-vm-bucket"
+    prefix  = "terraform/state/ns"
+    project = "terraform-project-vm>"
+    region  = "us-central1"
+  }
+}
