3e1dd437c18352a8708dde70fd291d28086251c7
Viktoriia Minosian <116222974+Vikmin2022@users.noreply.github.com>
2023-04-15 14:41:59 -0500

    Update main.tf

--- modules/Namespace/main.tf
+++ modules/Namespace/main.tf
@@ -2,8 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "terraform-project-vm-bucket"
     prefix  = "/terraform2/state/ns"
-    project = "terraform-project-vm"
-    region  = "Multi-region"
   }
 }
 
