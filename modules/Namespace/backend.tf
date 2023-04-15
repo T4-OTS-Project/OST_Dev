f53c3737edef6ec9f1701db10f50b8a8172df894
Viktoriia Minosian <116222974+Vikmin2022@users.noreply.github.com>
2023-04-15 14:14:57 -0500

    fixed backend

--- modules/Namespace/backend.tf
+++ modules/Namespace/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket  = "terraform-project-vm-bucket"
-    prefix  = "terraform2/state/ns"
+    prefix  = "/terraform2/state/ns"
     project = "terraform-project-vm"
     region  = "us-central1"
   }
