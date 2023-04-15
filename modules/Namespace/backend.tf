c00f9428f0b16c548fb977e14ea9d520f803b335
Viktoriia Minosian <116222974+Vikmin2022@users.noreply.github.com>
2023-04-15 14:32:46 -0500

    changed backend module  location

--- modules/Namespace/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraform-project-vm-bucket"
-    prefix  = "/terraform2/state/ns"
-    project = "terraform-project-vm"
-    region  = "Multi-region"
-  }
-}
--- modules/Namespace/main.tf
+++ modules/Namespace/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "gcs" {
+    bucket  = "terraform-project-vm-bucket"
+    prefix  = "/terraform2/state/ns"
+    project = "terraform-project-vm"
+    region  = "Multi-region"
+  }
+}
+
 module ots-ns {
     source = "./module"
     name = "ots-ns"
