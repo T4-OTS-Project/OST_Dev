18ef08b306594a07ae123e8912d6ac02affbf64c
Dmitry Blazhko <dblazhko@gmail.com>
2023-04-17 12:56:32 -0500

    Added vault module to create usrname/pass

---
+++ modules/vault/variable.tf
@@ -0,0 +1,10 @@
+variable "google_domain_name" {
+  type        = string
+  default     = "eastpost.org"
+  description = "Please use your domain"
+}
+
+variable "vault_token" {
+  type        = string
+  description = "Please use token provided"
+}
