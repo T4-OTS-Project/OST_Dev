7dc34cc480ba26ee97390223c19ba1f10bfe07be
atserge1 <vranceans@gmail.com>
2023-04-21 12:13:15 -0500

    aa

--- deployment/ots.tf
+++ deployment/ots.tf
@@ -10,17 +10,4 @@ module "ots-terraform-k8-helm-local" {
   deployment_chart_name = "ots-chart"
   deployment_namespace  = module.ots-k8-namespace.namespace
 
-  module "namespace" {
-  source      = "../modules/terraform-k8s-namespace"
-  name        = var.namespace
-  labels      = var.namespace_labels
-  annotations = var.namespace_annotations
-}
-
-
-module "application" {
-  source               = "../modules/terraform-helm-local"
-  deployment_name      = "${var.app_name}-${var.environment}"
-  deployment_namespace = var.namespace
-  deployment_path      = "../charts/application"
 }
