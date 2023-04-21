2a1fce205ff509c4ce062d280e3ce74edd512e7f
atserge1 <vranceans@gmail.com>
2023-04-21 11:16:08 -0500

    aa

--- deployment/ots.tf
+++ deployment/ots.tf
@@ -23,30 +23,4 @@ module "application" {
   deployment_name      = "${var.app_name}-${var.environment}"
   deployment_namespace = var.namespace
   deployment_path      = "../charts/application"
-  values_yaml          = <<EOF
-image:
-  repository: "${var.repository}"
-  tag: "${var.tag}"
-
-service:
-  port: "${var.app_port}"
-
-ingress:
-  enabled: true
-  annotations: 
-    ingress.kubernetes.io/ssl-redirect: "false"
-    kubernetes.io/ingress.class: nginx
-    cert-manager.io/cluster-issuer: letsencrypt-prod-dns01
-    acme.cert-manager.io/http01-edit-in-place: "true"
-  hosts:
-    - host: "${var.app_name}-${var.environment}.${var.domain}"
-      paths:
-        - path: /
-          pathType: ImplementationSpecific
-  tls: 
-  - secretName: "${var.app_name}-tls"
-    hosts:
-      - "${var.app_name}-${var.environment}.${var.domain}"
-    EOF
-}
 }
